3aa09dfa3efdadca67535a7eb01c83bf8635244c
Dmitry Ivanyushin <defance@gmail.com>
2017-02-16 18:00:12 +0300

    Починена работа с атрибутами по-умолчанию

--- xbserver_plugin/configuration.py
+++ xbserver_plugin/configuration.py
@@ -5,15 +5,18 @@ class IfmoXBServerConfiguration(object):
 
     def __setattr__(self, key, value):
         self.data[key] = value
+        
+    def __getattribute__(self, item):
+        data = super(IfmoXBServerConfiguration, self).__getattribute__("data")
+        return data.get(item, super(IfmoXBServerConfiguration, self).__getattribute__(item))
 
     def __getattr__(self, item):
-        try:
-            return self.data.get(item, getattr(self, item))
-        except KeyError:
-            if item not in self.required_fields:
-                return None
-            else:
-                raise
+        # Since we check in data dict prior to instance/parent dict, 
+        # we need to check this field in requirement whether to raise AttributeError
+        if item not in super(IfmoXBServerConfiguration, self).__getattribute__("required_fields"):
+            return None
+        else:
+            raise AttributeError(item)
 
     def update(self, configuration):
         self.data.update(configuration)
